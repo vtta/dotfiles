eb6cbd61da8b8a79a0dcaaf863b52cea781e9d3c
vtta <vtta0124@gmail.com>
2019-06-19 11:26:37 +0800

    disable `thefuck` stop using nvim as vim

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,7 +1,8 @@
 
-if type nvim >/dev/null 2>&1; then 
-    alias vim="nvim" 
-fi
+#if type /opt/local/bin/nvim >/dev/null 2>&1; then 
+#    alias vim="nvim" 
+#fi
 
+# eval $(thefuck --alias)
 alias whereismyheader="g++ -E -x c - -v < /dev/null"
 
