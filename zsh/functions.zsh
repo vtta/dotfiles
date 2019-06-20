5ffa908935829e01d024d1d22dc72f141dc35613
vtta <vtta0124@gmail.com>
2019-06-20 11:03:59 +0800

    set iterm profile according to system preferences 	- profiles needed: Dark, Light

---
+++ zsh/functions.zsh
@@ -0,0 +1,22 @@
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    set_iterm_profile() {
+        val=$(osascript -e "tell application \"System Events\" to tell appearance preferences to set res to dark mode" 2>/dev/null)
+        if [[ $val == "true" ]]; then
+            iterm_profile_toggle
+        fi
+    }
+
+    iterm_profile_toggle() {
+        if [[ $ITERM_PROFILE == "Light" ]]; then
+            echo -ne "\033]50;SetProfile=Dark\a"
+            export ITERM_PROFILE="Dark"
+        else
+            echo -ne "\033]50;SetProfile=Light\a"
+            export ITERM_PROFILE="Light"
+        fi
+    }
+
+    set_iterm_profile
+fi
+
