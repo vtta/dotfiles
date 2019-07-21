554df4e16acfa129e90f357fe290e213624903d8
vtta <vtta0124@gmail.com>
2019-07-21 12:29:39 +0800

    disable iterm profile auto-switching in ssh session

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -1,22 +1,22 @@
+iterm_profile_toggle() {
+    if [[ $ITERM_PROFILE == "Light" ]]; then
+        echo -ne "\033]50;SetProfile=Dark\a"
+        export ITERM_PROFILE="Dark"
+    else
+        echo -ne "\033]50;SetProfile=Light\a"
+        export ITERM_PROFILE="Light"
+    fi
+}
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    set_iterm_profile() {
-        val=$(osascript -e "tell application \"System Events\" to tell appearance preferences to set res to dark mode" 2>/dev/null)
-        if [[ $val == "false" ]]; then
-            iterm_profile_toggle
-        fi
-    }
+set_iterm_profile() {
+    val=$(osascript -e "tell application \"System Events\" to tell appearance preferences to set res to dark mode" 2>/dev/null)
+    if [[ $val == "false" ]]; then
+        iterm_profile_toggle
+    fi
+}
 
-    iterm_profile_toggle() {
-        if [[ $ITERM_PROFILE == "Light" ]]; then
-            echo -ne "\033]50;SetProfile=Dark\a"
-            export ITERM_PROFILE="Dark"
-        else
-            echo -ne "\033]50;SetProfile=Light\a"
-            export ITERM_PROFILE="Light"
-        fi
-    }
-
+if [[ ! -n $SSH_CONNECTION ]] && \
+    [[ "$(uname -s)" == "Darwin" ]]; then
     set_iterm_profile
 fi
 
